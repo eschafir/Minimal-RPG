3f551ef4d832da222156152b7bb421d41cdebffb
Esteban Schafir <esteban.schafir@gmail.com>
2018-06-10 11:19:41 -0300

    Enemy health bar (own way) THIS IS NOT HOW UDEMY EXPLAINS

---
+++ Assets/Enemies/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float maxHealthPoints = 100f;
+    float health = 100f;
+    private EnemyHealthBar healthBar;
+
+    void Start()
+    {
+        healthBar = GetComponentInChildren<EnemyHealthBar>();
+        print(healthBar);
+    }
+
+    public float healthAsPercentage {
+        get {
+            return health / maxHealthPoints;
+        }
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        healthBar.UpdateHealthBar();
+    }
+
+}
